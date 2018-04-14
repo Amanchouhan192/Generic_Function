ccc5a56f1d3ef333688f303306ae40e48b85521e
Amanchouhan192 <amanchouhan192@gmail.com>
2018-04-15 00:38:58 +0530

    adding another placeholder y

--- cpp/template/main.cpp
+++ cpp/template/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 using namespace std;
-template <class x>
-x big(x a , x b)
+template <class x, class y>
+x big(x a , y b)
 {
     if(a>b)
         return(a);
@@ -14,6 +14,8 @@ x big(x a , x b)
 int main()
 {   cout<<"biger is "<<big(5.6,8.9)<<endl;
     cout<<"biger is "<<big(6,9)<<endl;
-    cout << "Hello world!" << endl;
+    cout<<"adding another placeholder"<<endl;
+    cout<<"biger is "<<big(5,8.9)<<endl;
+
     return 0;
 }
