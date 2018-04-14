a6e2e04984089ab0a12e57ad4f23462d36b9b570
Amanchouhan192 <amanchouhan192@gmail.com>
2018-04-15 00:29:50 +0530

    c++ template

---
+++ cpp/template/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+template <class x>
+x big(x a , x b)
+{
+    if(a>b)
+        return(a);
+    else
+        return(b);
+
+}
+
+int main()
+{   cout<<"biger is "<<big(5.6,8.9)<<endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
